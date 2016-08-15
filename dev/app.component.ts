d388e4a6e0c3931199c434a58d73c0218c4813e1
KSaiPrakash <maverick16.sai@gmail.com>
2016-08-15 18:03:58 +0530

    Add files via upload

---
+++ dev/app.component.ts
@@ -0,0 +1,52 @@
+import {Component} from 'angular2/core';
+import {ControlGroup} from 'angular2/common';
+import {FirebaseService} from './firebase.service';
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: `<div class="flex">
+         <div id ="set user" class="container">
+           <h2>Set a User</h2>
+             <form {ngSubmit}="onSubmit(f)" #f="ngForm">
+               <div>
+                 <label form ="first-name">First Name</label>
+                 <input type="text" ngControl="firstName">
+               </div>
+            <div>
+             <label form ="last-name">Last Name</label>
+             <input type="text" ngControl="lastName">
+            </div>
+             <button type="submit">Submit</button>
+             </form>
+             </div>
+             <div id ="get-user" class ="container">
+             <h2> Get User </h2>
+            <button (click)="onGetUser()">Get User</button>
+            </div>
+            </div>
+            <div class ="container" id="response">Responses: {{response}}</div>
+            `,
+    providers: [FirebaseService]
+})
+
+export class AppComponent {
+    response: string;
+
+    constructor(private _firebaseService: FirebaseService){}
+
+    onSubmit(form:ControlGroup){
+        this._firebaseService.setUser(form.value.firstName, form.value.lastName)
+        .subscribe(
+            user => this.response = JSON.stringify(user),
+            error => console.log(error)
+        );
+    }
+
+    onGetUser(){
+        this._firebaseService.getUser().subscribe(
+            user => this.response = JSON.stringify(user),
+            error => console.log(error)
+        );
+    }
+}
---
+++ dev/firebase.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from "angular2/core";
+import {Http} from "angular2/http";
+import {last} from "rxjs/operator/last";
+import 'rxjs/Rx';
+
+@Injectable()
+
+export class FirebaseService{
+  constructor(private _http:Http){}
+ 
+  setUser(firstName: string, lastName:string){
+
+    const body =JSON.stringify({firstName: firstName, lastName:lastName});
+
+    return this._http.put('https://my-first-app-28bf4.firebaseio.com/user.json', body)
+    .map(response => response.json());
+  }
+  getUser(){
+   return this._http.get('https://my-first-app-28bf4.firebaseio.com/user.json')
+   .map(response => response.json());
+  }
+}
